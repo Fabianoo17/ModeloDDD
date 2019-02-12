d0b0ca91431b8299dd733bb85d20af1645d1ba45
Fabiano Silva <binnew19@gmail.com>
2019-02-11 22:13:25 -0200

    Modelo de DDD

---
+++ Infrastructure/Data/Repositories/ClienteRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Infrastructure.Data.Repositories
+{
+    class ClienteRepository : RepositoryBase<Cliente>, IClienteRepository
+    {
+    }
+}
---
+++ Infrastructure/Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Interfaces;
+using Infrastructure.Data.Contexto;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+namespace Infrastructure.Data.Repositories
+{
+    public class RepositoryBase<TEntity> : IDisposable, IRepositoryBase<TEntity> where TEntity : class
+    {
+        protected ModeloDDDContext Db = new ModeloDDDContext();
+
+        public void Add(TEntity obj)
+        {
+            Db.Set<TEntity>().Add(obj);
+            Db.SaveChanges();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return Db.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+           return Db.Set<TEntity>().ToList();
+        }
+
+        public void Update(TEntity Obj)
+        {
+            Db.Entry(Obj).State = EntityState.Modified;
+            Db.SaveChanges();
+        }
+
+        public void Remove(TEntity Obj)
+        {
+            Db.Set<TEntity>().Remove(Obj);
+            Db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+}
